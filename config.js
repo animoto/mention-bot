7e1053994648b67db41554c72dd4eb843cfe4530
Lincoln Ritter <lincoln@animoto.com>
2018-07-27 10:19:23 -0400

    WIP: cant seem to make jest read what it should

--- config.js
+++ config.js
@@ -1,14 +1,41 @@
 var exports = module.exports = {};
 
-var packageConfig = require('./package.json').config;
-var configJson = require('./config.json');
+function getPackageConfig() {
+  return require('./package.json').config;
+}
 
-var mergedConfig = Object.assign(packageConfig, configJson);
+function getConfigJSON() {
+  return require('./config.json');
+}
 
-exports.github = {
-  host: process.env.GHE_HOST || mergedConfig.gheHost || 'github.com',
-  apiHost: process.env.GHE_API_HOST || mergedConfig.gheHost || 'api.github.com',
-  protocol: process.env.GHE_PROTOCOL || mergedConfig.gheProtocol || 'https',
-  pathPrefix: process.env.GHE_PATH_PREFIX || mergedConfig.ghePathPrefix,
-  port: process.env.GHE_PORT || mergedConfig.ghePort || 443
-};
+
+
+function buildConfig() {
+  var packageConfig = getPackageConfig();
+  var configJson = getConfigJSON();
+  var mergedConfig = Object.assign(packageConfig, configJson);
+  var config = {}
+  config.github = {
+    host: process.env.GHE_HOST || mergedConfig.gheHost || 'github.com',
+    apiHost: process.env.GHE_API_HOST || mergedConfig.gheHost || 'api.github.com',
+    protocol: process.env.GHE_PROTOCOL || mergedConfig.gheProtocol || 'https',
+    pathPrefix: process.env.GHE_PATH_PREFIX || mergedConfig.ghePathPrefix || null,
+    port: process.env.GHE_PORT || mergedConfig.ghePort || 443
+  };
+
+  config.gh = config.github;
+
+  return config;
+}
+
+exports = Object.assign(exports,buildConfig());
+
+// exports.github = {
+//   host: process.env.GHE_HOST || mergedConfig.gheHost || 'github.com',
+//   apiHost: process.env.GHE_API_HOST || mergedConfig.gheHost || 'api.github.com',
+//   protocol: process.env.GHE_PROTOCOL || mergedConfig.gheProtocol || 'https',
+//   pathPrefix: process.env.GHE_PATH_PREFIX || mergedConfig.ghePathPrefix || null,
+//   port: process.env.GHE_PORT || mergedConfig.ghePort || 443
+// };
+
+// exports.gh = exports.github;
